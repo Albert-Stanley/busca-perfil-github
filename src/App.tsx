a19d39b723b4829f7902a44d7f080326d285d54c
Albert Stanley <153841479+Albert-Stanley@users.noreply.github.com>
2025-04-21 10:40:49 -0300

    feat: PageTitle ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ellipse1 from "./assets/ellipse1.svg";
 import ellipse2 from "./assets/ellipse2.svg";
 import dottedLayer from "./assets/Camada_1.svg";
+import PageTitle from "./components/PageTitle";
 
 export default function GitHubProfileLayout() {
   return (
@@ -27,8 +28,11 @@ export default function GitHubProfileLayout() {
       />
 
       {/* Container preto central */}
-      <div className="z-10 bg-black w-[90%] max-w-[1156px] h-[80%] max-h-[537px] rounded-lg p-6 md:p-8 shadow-xl flex flex-col justify-center items-center">
-        <p className="text-gray-400 text-xl text-center">Aguardando busca...</p>
+      <div className="z-10 bg-black w-[90%] max-w-[1156px] h-[80%] max-h-[537px] rounded-lg p-6 md:p-8 shadow-xl flex flex-col justify-start items-center">
+        <PageTitle></PageTitle>
+        <p className="text-gray-400 text-xl text-center  mt-4">
+          Aguardando busca...
+        </p>
       </div>
     </div>
   );
---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,12 @@
+import GitHubIcon from "../assets/github-icon.svg";
+import GitHubLogo from "../assets/github-logo.svg";
+
+export default function PageTitle() {
+  return (
+    <div className="text-center flex justify-center gap-x-4 ">
+      <img src={GitHubIcon} alt="Icone GitHub" />
+      <h1 className="font-semibold text-6xl">Perfil</h1>
+      <img src={GitHubLogo} alt="Logo GitHub" />
+    </div>
+  );
+}
